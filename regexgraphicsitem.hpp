eb9ca47d163ea8965bab0a72c86203548a8432b3
Alex Elliott <alex@alex-elliott.co.uk>
2009-12-22 13:03:34 +0000

    Minor update to base class files

--- regexgraphicsitem.hpp
+++ regexgraphicsitem.hpp
@@ -1,3 +1,23 @@
+/**
+ * This file is part of Expression editor
+ *
+ * Expression editor is Copyright 2009 Alex Elliott <alex@alex-elliott.co.uk>
+ *
+ * Expression editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Expression editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Expression editor.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef REGEXGRAPHICSITEM_HPP
 #define REGEXGRAPHICSITEM_HPP
 
