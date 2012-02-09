2761445d630df844e9106d19bad6e2c7ec5ca9c9
Alex Elliott <alex@alex-elliott.co.uk>
2012-02-09 20:03:11 +0000

    Shouldn't commit the binaries

--- android/bin/Expressioneditor-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Alex\Documents\Github\expressioneditor-android\android\bin\Expressioneditor-debug-unaligned.apk : \
-C:\Users\Alex\Documents\Github\expressioneditor-android\android\bin\Expressioneditor.ap_ \
-C:\Users\Alex\Documents\Github\expressioneditor-android\android\bin\classes.dex \
-C:\Users\Alex\Documents\Github\expressioneditor-android\android\libs\armeabi\gdbserver \
-C:\Users\Alex\Documents\Github\expressioneditor-android\android\libs\armeabi\libexpressioneditor.so \
