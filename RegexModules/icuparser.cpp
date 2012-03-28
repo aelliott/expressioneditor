0158057b1bf7185c8189895170b135fec7e5e9c9
Alex Elliott <alex@alex-elliott.co.uk>
2012-03-28 16:43:06 +0100

    Missed off a backslash. \. => \\.

--- RegexModules/icuparser.cpp
+++ RegexModules/icuparser.cpp
@@ -198,7 +198,7 @@ void IcuParser::handleToken(RegexpToken token)
 {
     QRegExp rx;
     QRegExp characterClass;
-    QRegExp bracketExpressionLiteral("[^\\\\[\\]\.]|\\\\[^a-zA-Z0-9]");
+    QRegExp bracketExpressionLiteral("[^\\\\[\\]\\.]|\\\\[^a-zA-Z0-9]");
     QRegExp bracketExpressionRange(_syntax[T_BRACKET_EXPRESSION_RANGE]);
     QRegExp flagClose;
     Token *openingToken;
